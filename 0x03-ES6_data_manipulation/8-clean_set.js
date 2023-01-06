137aa3a8dfee42b5e8d8b7b7d591b7f0d239aa72
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-01-06 07:47:16 +0100

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  const cleanedSet = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      cleanedSet.push(value.slice(startString.length));
+    }
+  }
+  return cleanedSet.join('-');
+}
