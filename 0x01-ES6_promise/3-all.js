c3413a81a0665a7675f3de52c0fbc69f4230b8a5
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-01-15 12:20:59 +0100

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import {uploadPhoto, createUser} from "./utils.js";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((resp) => {
+    const { body } = resp[0];
+    const { firstName } = resp[1];
+    const { lastName } = resp[1];
+    console.log(`${body} ${firstName} ${lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  })
+}
