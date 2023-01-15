53245e4bf0387417424de98c4f3b04386460b91d
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-01-15 12:27:08 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,5 @@
-import {uploadPhoto, createUser} from "./utils.js";
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((resp) => {
