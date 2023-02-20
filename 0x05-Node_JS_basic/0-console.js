23c4b448be750edd3a10b7f4af8cb734579c5b4b
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-02-21 00:23:49 +0100

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * print in STDOUT the string argument
+ * @author Djogona Mahamat
+ * @param {string} str - string to be printed
+ * return {void}
+ */
+function displayMessage(str) {
+  console.log(str);
+}
+
+module.exports = displayMessage;
